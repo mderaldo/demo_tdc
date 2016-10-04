7786513801e4110be5fcaf6caa8cff8983d06e8d
Melina Deraldo <mderaldo@thoughtworks.com>
2016-10-03 22:29:55 -0300

    Setup basic tools

--- cookbooks/tdc/recipes/default.rb
+++ cookbooks/tdc/recipes/default.rb
@@ -6,3 +6,31 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+# update package database
+execute "apt-get update"
+
+# install packages
+package "telnet"
+package "postfix"
+package "curl"
+package "git-core"
+package "zlib1g-dev"
+package "libssl-dev"
+package "libreadline-dev"
+package "libyaml-dev"
+package "libsqlite3-dev"
+package "sqlite3"
+package "libxml2-dev"
+package "libxslt1-dev"
+package "libpq-dev"
+package "build-essential"
+package "tree"
+
+# set timezone
+bash "set timezone" do
+  code <<-EOH
+    echo 'US/Pacific-New' > /etc/timezone
+    dpkg-reconfigure -f noninteractive tzdata
+  EOH
+  not_if "date | grep -q 'PDT\|PST'"
+end
