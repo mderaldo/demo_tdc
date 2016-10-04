492331662353db1d8be7906ef71efd198840c16b
Melina Deraldo <mderaldo@thoughtworks.com>
2016-10-04 13:08:09 -0300

    Add unit test

--- cookbooks/tdc/spec/recipes/default_spec.rb
+++ cookbooks/tdc/spec/recipes/default_spec.rb
@@ -2,9 +2,11 @@ require 'spec_helper'
 
 describe 'tdc::default' do
   cached(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+
   before do
     stub_command("date | grep -q 'PDT|PST'").and_return('')
   end
+  
   it 'executes apt-get update' do
     expect(chef_run).to run_execute('apt-get update')
   end
@@ -20,13 +22,11 @@ describe 'tdc::default' do
    expect(chef_run).to install_package('curl')
   end
 
-  it 'instal postfix package' do
+  it 'instal git-core package' do
     expect(chef_run).to install_package('git-core')
   end
 
-  it 'instal postfix package' do
+  it 'instal tree package' do
     expect(chef_run).to install_package('tree')
   end
-
-  
 end
