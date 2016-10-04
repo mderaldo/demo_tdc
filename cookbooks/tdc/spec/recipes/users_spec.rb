51a68d7e9e01dd4c976711d120a95eebf6921d6c
Melina Deraldo <mderaldo@thoughtworks.com>
2016-10-04 17:53:26 -0300

    Add stub t bash commands

--- cookbooks/tdc/spec/recipes/users_spec.rb
+++ cookbooks/tdc/spec/recipes/users_spec.rb
@@ -4,6 +4,7 @@ describe 'tdc::users' do
 	cached(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
 	before(:each) do
+		stub_command("grep -xq '%tdc_group ALL=(ALL) ALL' /etc/sudoers").and_return('')
 		chef_run.converge(described_recipe)
 	end
 
